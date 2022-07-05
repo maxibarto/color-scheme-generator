2edfd590f367d119c02b88beaf3e4dbd5e5f386f
Maxi Bartolozzi <maxibartolozzi@gmail.com>
2022-07-04 21:42:28 -0300

    refactor

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
+export {renderPallete, copyColorsCode} from './utils.js'
+
 const colorForm = document.getElementById('color-form')
 const colorInput = document.getElementById('color-input')
 const schemeList = document.getElementById('scheme-list')
 const schemeBtn = document.getElementById('scheme-btn')
-const colorContainer = document.getElementById('color-container')
-const colorFooter = document.getElementById('color-footer')
-const colorImg = document.getElementById('color-img')
 
 
 // Active the button to get the color from the API
@@ -13,39 +12,6 @@ const activeGetSchemeBtn = (e) => {
         schemeBtn.disabled = false
     })
 }
-
-// Render pallete of colors
-const renderPallete = () => {
-    let palleteHtml = ''
-    let footerHtml = ''
-    for(color of colorPallete){
-        palleteHtml += `
-            <div style="background-color:${color}"></div>
-        `
-        footerHtml += `
-            <p>${color}<span>${color} copied to Clipboard!</span></p>
-        `
-    }
-    colorContainer.innerHTML = palleteHtml
-    colorFooter.innerHTML = footerHtml
-    schemeBtn.disabled = true
-}
-
-// Copy text to the clipboard
-const copyColorsCode = () => {
-    let colorCode = colorFooter.getElementsByTagName('p')
-    let colorCopied = colorFooter.getElementsByTagName('span')
-    for(let i = 0; i < colorCode.length; i++){
-        colorCode[i].addEventListener('click', () => {
-            navigator.clipboard.writeText(colorCode[i].textContent)
-            colorCopied[i].style.visibility = 'visible'
-            setTimeout(() => {
-                colorCopied[i].style.visibility = 'hidden'
-            }, 1000)
-        })
-    }
-}
- 
 
 let colorPallete = []
 colorForm.addEventListener('submit', e =>{
